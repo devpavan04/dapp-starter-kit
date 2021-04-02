e5c78a00fc0e35a07edfb78861223297377c2e03
pavanvsoratur <pavanvsoratur@gmail.com>
2021-04-02 15:11:23 +0530

    added test folder

---
+++ test/Message.test.js
@@ -0,0 +1,23 @@
+const { assert } = require("chai");
+
+const Message = artifacts.require("./Message.sol");
+
+require("chai").use(require("chai-as-promised")).should();
+
+contract("Crypto Boys", async (accounts) => {
+  let message;
+
+  before(async () => {
+    message = await Message.deployed();
+  });
+
+  describe("Deployment", async () => {
+    it("contract has an address", async () => {
+      const address = await message.address;
+      assert.notEqual(address, 0x0);
+      assert.notEqual(address, "");
+      assert.notEqual(address, null);
+      assert.notEqual(address, undefined);
+    });
+  });
+});
